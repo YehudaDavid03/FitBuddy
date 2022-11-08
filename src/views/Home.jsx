9dec6b80d209a189246af399bd6fa9f126a2b27c
Yehuda Mordechai David <yehudadavid@yehudas-mbp.mynetworksettings.com>
2022-11-08 18:29:16 -0500

    home page new design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 import useAxios from "../components/useAxios"
 import LoadingWheel from "../components/LoadingWheel"
 
-import runningImage from "../assets/running_image.png"
 import profileOne from "../assets/profile-one.png"
 import profileTwo from "../assets/profile-two.png"
 import profileThree from "../assets/profile-three.png"
+import display from "../assets/display.png"
 
 const Home = () => {
   // Message to Fitbudy variables for suggestions
@@ -46,8 +46,8 @@ const Home = () => {
     } else {  }
   }, [contactApiResponse, contactApiError])
 
-  // Testimonials to be renderd out 
-  const testimonials = [
+  // Testimonials to be rendered out 
+  const testimonialsArr = [
     {
       testimonialId: "780erhIOA",
       testimonialImage: profileOne,
@@ -70,6 +70,33 @@ const Home = () => {
       testimonialDescription: "As an avid runner and someone who prefers to run solo, I wasn't sure if FitBuddy was going to be for me, but I was keen to see if anyone could benefit from running with me, or learning from my personal experiences. As an experienced runner I like helping others achieve their goals, regardless of their ability or the distance.",
     }
   ]
+
+  const reasonsArr = [
+    {
+      reasonId: "890sdkJKM",
+      reasonIcon: "groups",
+      reasonHeader: "Meet New People",
+      reasonText: "Meet like minded people who have a passion for exercise. We use your geolocation to match you with other users in your area, so you'll always be able to meet with your exercise buddies. Become friends with your partners and easily stay in touch with our built in messaging system."
+    },
+    {
+      reasonId: "812hkqYIO",
+      reasonIcon: "show_chart",
+      reasonHeader: "Stay Motivated",
+      reasonText: "Stay motivated with your exercise buddy. Having a shared goal and working towards it together is a lot more motivational than trying to exercise on a solo mission. Working out together also has the benefit that you can pace each other and push harder than you would if you didn't have a partner at your side."
+    },
+    {
+      reasonId: "944hsdKMK",
+      reasonIcon: "explore",
+      reasonHeader: "Explore New Trails",
+      reasonText: "Have your partner show you their favorite trails and do something different to the ordinary. If you are traveling, have a local guide you and be blown away by the breath taking views that you would have otherwise missed."
+    },
+    {
+      reasonId: "245hjsAZX",
+      reasonIcon: "military_tech",
+      reasonHeader: "Reach Your Goals",
+      reasonText: "Reach your goals with a exercise partner. Having a partner will increase your consistency. Recent research shows that committing to an accountability partner increases the likelihood by 80% that you will follow through with your commitment. Exercising more often will ultimately boost your condition."
+    }
+  ]
   
   return (
     contactApiLoading ? 
@@ -81,20 +108,18 @@ const Home = () => {
     (
       <div className="home-main">
         <nav>
-        <div>
+          <div>
             <Link style={{textDecoration: "none"}} to="/"><h1>FitBuddy</h1></Link>
           </div>
 
           <div>
-            <Link style={{textDecoration: "none"}} to="/login"><button>Login</button></Link>
-            <Link style={{textDecoration: "none"}} to="/sign-up"><button>Register</button></Link>
+            <Link className="home-main-btn" style={{textDecoration: "none"}} to="/login">Login</Link>
+            <Link className="home-main-btn" style={{textDecoration: "none"}} to="/sign-up">Register</Link>
           </div>
         </nav>
 
-        {/* About */}
-        <header>
-          <img style={{width: "100%"}} src={runningImage} alt="" />
-          
+        {/* Page Start */}
+        <header className="home-main-header">
           <div>
             <span>Find a sports partner</span>
             <p>
@@ -107,77 +132,74 @@ const Home = () => {
           </div>
         </header>
 
-        {/* Reasons */}
-        <section className="reasons">
+        {/* Chart Statistics */}
+        <div className="home-main-chart">
+          <span>100's of people near you are looking for a workout buddy...</span>
+          <p>
+            FitBuddy is an web application that helps you find a workout buddy near you. 
+            The app will suggest people that have similar preferences and routines as you, 
+            thus increasing the probability that you'll be an excellent match. 
+            You will be able to effortlessly arrange your workout's and set a meeting point, 
+            allowing you to focus on what is most important: exercising more.
+          </p>
           <div>
-            <h1><span>5 REASONS</span> TO USE FitBuddy.com</h1>
+            <img src={display} />
           </div>
+        </div>
 
-          <div>
-            <footer>
-              <span className="material-icons">military_tech</span>
-              <p>Achieve your goals quicker with a training partner</p>
-            </footer>
-
-            <footer>
-              <span className="material-icons">leaderboard</span>
-              <p>Get more from your exercise with a partner</p>
-            </footer>
-
-            <footer>
-              <span className="material-icons">connect_without_contact</span>
-              <p>Go the distance with a running partner</p>
-            </footer>
-
-            <footer>
-              <span className="material-icons">place</span>
-              <p>Find a training partner at your destination when you travel</p>
-            </footer>
-
-            <footer>
-              <span className="material-icons">filter_center_focus</span>
-              <p>Get focused! - Find a training partner to get you motivated</p>
-            </footer>
-          </div>
-        </section>
-
-        {/* Testimonials */}
-        <section className="testimonials">
-          <h1>testimonials</h1>
+        {/* Reasons To Use */}
+        <div className="home-main-reasons">
+          <h1><span>5 REASONS</span> TO USE FitBuddy.com</h1>
 
           <main>
             {
-              testimonials.map((item) => {
+              reasonsArr.map((item) => {
                 return (
-                  <footer key= {item.testimonialId}>
-                    <div>
-                      <img src={item.testimonialImage} />
-                      <h1>{item.testimonialFullName}</h1>
+                  <div key={item.reasonId}>
+                    <span className="material-icons">{item.reasonIcon}</span>
+                    <h1>{item.reasonHeader}</h1>
+                    <p>{item.reasonText}</p>
+                  </div>
+                )
+              })
+            }
+          </main>
+        </div>
+
+        {/* Testimonials */}
+        <div className="home-main-testimonials">
+          <h1><span>FITBUDDY </span> ALREADY HAS RAVING FANS!</h1>
+
+          <main>
+            {
+              testimonialsArr.map((item) => {
+                return (
+                  <div key={item.testimonialId}>
+                    <span style={{color: "var(--main)", fontSize: "62px"}} className="material-icons">format_quote</span>
+                    <p>{item.testimonialDescription}</p>
+                    <p>{item.testimonialFullName}</p>
+                    <section>
                       {
                         (() => {
                           const starCount = []
 
                           for (let i = 0; i < item.testimonialStars; i++) {
-                            starCount.push(<span className="material-icons">star</span>)
+                            starCount.push(<span style={{color: "#ffcd3c", fontSize: "24px"}} className="material-icons">star</span>)
                           }
 
                           return starCount
                         })()
                       }
-                    </div>
-          
-                    <div>
-                      <p>{item.testimonialDescription}</p>
-                    </div>
-                  </footer>
+                    </section>
+                  </div>
                 )
               })
             }
           </main>
-        </section>
+        </div>
 
-        {/* Contact */}
-        <section className="contact">
+        {/* Contact Section */}
+        <div className="home-main-contact">
           <h1>Send us a message</h1>
 
           <input 
@@ -187,7 +209,7 @@ const Home = () => {
             value={contact.fullName}
             onChange={(e) => { setContact({ ...contact, [e.target.name]: e.target.value }) }}
           />
-          
+
           <input 
             type="text"
             name="emailAddress" 
@@ -195,7 +217,7 @@ const Home = () => {
             value={contact.emailAddress}
             onChange={(e) => { setContact({ ...contact, [e.target.name]: e.target.value }) }}
           />
-          
+
           <input 
             type="text"
             name="subject" 
@@ -207,11 +229,10 @@ const Home = () => {
           <textarea type="text" name="message" placeholder="Message" value={contact.message} onChange={(e) => { setContact({ ...contact, [e.target.name]: e.target.value }) }}></textarea>
 
           <button onClick={() => { contactApi() }} >send</button>
-        </section>
+        </div>
 
-        {/* Footer */}
-        <footer className="footer">
-          <p><Link style={{textDecoration: "none", fontWeight: "600"}} to="/login">Login -</Link><Link style={{textDecoration: "none", fontWeight: "600"}} to="/sign-up"> Register</Link></p>
+        {/* Bottom Nav */}
+        <footer className="home-main-footer">
           <h1>{"Copyright"} <span className="material-icons">copyright</span>{`FitBuddy ${new Date().getFullYear() - 4} - ${new Date().getFullYear()}`} All Rights Reserved</h1>
         </footer>
       </div>
